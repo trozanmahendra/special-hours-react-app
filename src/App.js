506369eb4f2b84aa33fcc3551318b2d112b1eba3
Mahendra Goud <mahendra.goud@proeffective.in>
2024-10-15 17:58:48 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import hoursData from "./data";
 import './App.css';
 
 function App() {
+  const [specialHours, setSpecialHours] = useState(hoursData.specialHours);
+  const [hours, setHours] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (hours) {
+      const newSpecialHour = {hours };
+      setSpecialHours([...specialHours,newSpecialHour]);
+      setHours("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Supply House Business Hours</h1>
+      <section>
+        <h2>Regular Hours</h2>
+        <ul>
+          {Object.entries(hoursData.regularHours).map(([day, hours]) => (
+            <li key={day}>
+              {day.charAt(0).toUpperCase() + day.slice(1)}: {hours}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>Special Hours</h2>
+        <ul>
+          {specialHours.length > 0 ? (
+            specialHours.map((special, index) => (
+              <li key={index} className="special-hours">
+                {special.hours}
+              </li>
+            ))
+          ) : (
+            <li>No special hours configured.</li>
+          )}
+        </ul>
+      </section>
+      <section>
+        <h2>Add Special Hours</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Hours:
+              <input
+                type="text"
+                value={hours}
+                placeholder="2:30 AM - 5:30 PM"
+                onChange={(e) => setHours(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <button type="submit">Add</button>
+        </form>
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,18 @@
+const hoursData = {
+    regularHours: {
+      monday: "9 AM - 5 PM",
+      tuesday: "9 AM - 5 PM",
+      wednesday: "9 AM - 5 PM",
+      thursday: "9 AM - 5 PM",
+      friday: "9 AM - 5 PM",
+      saturday: "Closes",
+      sunday: "Closed",
+    },
+    specialHours: [
+      {
+        hours: "Closed on holidays",
+      },
+    ],
+  };
+
+  export default hoursData;
